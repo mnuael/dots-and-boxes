3de9c3738c0ee8e99fe91cfdbd2a8b8b4410b43f
Manuel Mangalathu <manuelmangalathu@gmail.com>
2026-06-15 22:40:24 +0200

    create 4x4 dots and lines with buttons

--- src/main/java/com/game/Game.java
+++ src/main/java/com/game/Game.java
@@ -1,5 +1,8 @@
 package com.game;
 
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
@@ -17,6 +20,37 @@ class Game {
             terminal.putString("Welcome to dots and boxes.");
             terminal.putCharacter('\n');
 
+            var screen = new TerminalScreen(terminal);
+            screen.startScreen();
+
+            var panel = new Panel();
+            GridLayout gridLayout = new GridLayout(3);
+            panel.setLayoutManager(gridLayout);
+
+
+            var button = new Button("-", new Runnable() {
+                @Override
+                public void run() {}
+            });
+            var button2 = new Button("|", new Runnable() {
+                @Override
+                public void run() {}
+            });
+            panel.addComponent(new Label("o"));
+            panel.addComponent(getButton("-"));
+            panel.addComponent(new Label("o"));
+            panel.addComponent(getButton("|"));
+            panel.addComponent(new EmptySpace());
+            panel.addComponent(getButton("|"));
+            panel.addComponent(new Label("o"));
+            panel.addComponent(getButton("-"));
+            panel.addComponent(new Label("o"));
+
+            var window = new BasicWindow();
+            window.setComponent(panel);
+
+            var gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
+            gui.addWindowAndWait(window);
 
             var gridStr = grid.toString();
             String[] split = gridStr.split("\n");
@@ -29,5 +63,16 @@ class Game {
         } catch (Exception e) {
             throw e;
         }
+    }
+
+    static Button getButton(String toRenderSymbol) {
+        Button button = new Button(" ", () -> {
+        }).setRenderer(new Button.FlatButtonRenderer());
+        button.addListener( (b) -> {
+            b.setEnabled(false);
+            Button.Listener listener = btn -> btn.setEnabled(false);
+            b.setLabel(toRenderSymbol);
+        });
+        return button;
     }
 }
